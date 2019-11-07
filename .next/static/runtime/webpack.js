4c7e4c0203e2c7169084fa8f65abf820b66cee54
Wallace Saturnino <wallacesat@live.co.uk>
2019-11-07 15:36:06 -0300

    COnfigure global document

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d267979bbcb51118c0e";
+/******/ 	var hotCurrentHash = "dba98ddfcc4d86a97e29";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
